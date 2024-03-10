3344b595cc7a1ff9ed52c3ee5c5a1f3e1eac9ede
ryuji0123 <rokuda1011@gmail.com>
2024-03-10 09:39:35 +0900

    [iOS] Implement Footer tab skelton and New Action Button skelton.

---
+++ swift/Twitter-iOS/Twitter-iOS/HomeFooterTabView.swift
@@ -0,0 +1,24 @@
+//
+//  HomeFooterTabView.swift
+//  Twitter-iOS
+//
+//  Created by 奥田遼 on 2024/03/09.
+//
+
+import SwiftUI
+
+struct HomeFooterTabView: View {
+  var body: some View {
+    TabView {
+      Text("X").tabItem { Image(systemName: "house") }
+      Text("Search").tabItem { Image(systemName: "magnifyingglass") }
+      Text("Communities").tabItem { Image(systemName: "person.2") }
+      Text("Notifications").tabItem { Image(systemName: "bell") }
+      Text("Messages").tabItem { Image(systemName: "envelope") }
+    }
+  }
+}
+
+#Preview {
+  HomeFooterTabView()
+}
