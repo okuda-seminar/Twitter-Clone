cd48e2e6309d0474aca3d343a12c1b705064bdce
ryuji0123 <rokuda1011@gmail.com>
2024-03-10 10:24:20 +0900

    [iOS] Add ForYouTabView skelton.

---
+++ swift/Twitter-iOS/Twitter-iOS/Features/Home/ForYouTabView.swift
@@ -0,0 +1,33 @@
+//
+//  ForYouTabView.swift
+//  Twitter-iOS
+//
+//  Created by 奥田遼 on 2024/03/10.
+//
+
+import SwiftUI
+
+struct ForYouTabView: View {
+  private let fakeTweets: [Tweet] = {
+    var tweets: [Tweet] = []
+    for _ in 0..<30 {
+      tweets.append(fakeTweet)
+    }
+    return tweets
+  }()
+
+  var body: some View {
+    ScrollView {
+      VStack {
+        ForEach(fakeTweets) { fakeTweet in
+          Divider()
+          TweetCellView(userIcon: fakeTweet.userIcon, userName: fakeTweet.userName, bodyText: fakeTweet.bodyText)
+        }
+      }
+    }
+  }
+}
+
+#Preview {
+  ForYouTabView()
+}
