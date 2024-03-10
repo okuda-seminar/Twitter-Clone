a4a880fca089e74e37cc0fb11eb023976e57f8d9
ryuji0123 <rokuda1011@gmail.com>
2024-03-10 10:24:20 +0900

    [iOS] Add ForYouTabView skeleton.

---
+++ swift/Twitter-iOS/Twitter-iOS/Features/Home/TweetCellView.swift
@@ -0,0 +1,31 @@
+//
+//  TweetCellView.swift
+//  Twitter-iOS
+//
+//  Created by 奥田遼 on 2024/03/10.
+//
+
+import SwiftUI
+
+let fakeTweet = Tweet(bodyText: "Use a binding to create a two-way connection between a property that stores data, and a view that displays and changes the data. ", userIcon: Image(systemName: "apple.logo"), userName: "Apple")
+
+struct TweetCellView: View {
+  // TODO: https://github.com/okuda-seminar/Twitter-Clone/issues/23 - Polish tweet cell view UI.
+  var userIcon: Image
+  var userName: String
+  var bodyText: String
+
+  var body: some View {
+    HStack(alignment: .top) {
+      userIcon
+      VStack(alignment: .leading) {
+        Text(userName)
+        Text(bodyText)
+      }
+    }
+  }
+}
+
+#Preview {
+  TweetCellView(userIcon: fakeTweet.userIcon, userName: fakeTweet.userName, bodyText: fakeTweet.bodyText)
+}
---
+++ swift/Twitter-iOS/Twitter-iOS/Features/Tweet/Tweet.swift
@@ -0,0 +1,15 @@
+//
+//  Tweet.swift
+//  Twitter-iOS
+//
+//  Created by 奥田遼 on 2024/03/10.
+//
+
+import SwiftUI
+
+struct Tweet: Identifiable {
+  let id = UUID()
+  let bodyText: String
+  let userIcon: Image
+  let userName: String
+}
