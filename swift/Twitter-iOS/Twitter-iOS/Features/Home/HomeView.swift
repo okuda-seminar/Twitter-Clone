f337edefe5650b12215273ed83e75866860672b8
ryuji0123 <rokuda1011@gmail.com>
2024-03-15 21:20:16 +0900

    [iOS] Refactor buttons and add underbar to indicate which tab is currently selected.

--- swift/Twitter-iOS/Twitter-iOS/Features/Home/HomeView.swift
+++ swift/Twitter-iOS/Twitter-iOS/Features/Home/HomeView.swift
@@ -8,27 +8,56 @@
 import SwiftUI
 
 struct HomeView: View {
-  private var headerButtonWidth = UIScreen.main.bounds.width / 2
+  @Namespace var animation
+  @State var currentTab = "For you"
 
   var body: some View {
     VStack {
       // TODO: https://github.com/okuda-seminar/Twitter-Clone/issues/27 - Enable header tab buttons in Home to switch corresponding views.
       HStack {
-        Button(action: {}, label: {
-          Text("For you")
-            .font(.headline)
-            .foregroundStyle(.primary)
-        })
-        .buttonStyle(HeaderTabButtonStyle(buttonWidth: headerButtonWidth))
-        Button(action: {}, label: {
-          Text("Following")
-            .font(.headline)
-            .foregroundStyle(.primary)
-        })
-        .buttonStyle(HeaderTabButtonStyle(buttonWidth: headerButtonWidth))
+        HomeHeaderButton(currentTab: $currentTab, animation: animation, title: "For you")
+
+        HomeHeaderButton(currentTab: $currentTab, animation: animation, title: "Following")
       }
       ForYouTabView()
     }
+  }
+}
+
+struct HomeHeaderButton: View {
+  @Binding var currentTab: String
+  var animation: Namespace.ID
+  var title: String
+
+  private enum LayoutConstant {
+    static let buttonWidth = 120.0
+    static let underBarHeight = 3.0
+  }
+
+  var body: some View {
+    Button(action: {
+      withAnimation {
+        currentTab = title
+      }
+    }, label: {
+      VStack {
+        Text(title)
+          .font(.headline)
+          .foregroundStyle(.primary)
+
+        if currentTab == title {
+          Capsule()
+            .fill(Color.blue)
+            .frame(height: LayoutConstant.underBarHeight)
+            .matchedGeometryEffect(id: "TAB", in: animation)
+        } else {
+          Capsule()
+            .fill(Color.clear)
+            .frame(height: LayoutConstant.underBarHeight)
+        }
+      }
+    })
+    .buttonStyle(HeaderTabButtonStyle(buttonWidth: LayoutConstant.buttonWidth))
   }
 }
 
