5c629fb728ad7566c910bfa460a5c0f187726f50
Ryuju0911 <pinebook0911@gmail.com>
2024-03-17 21:47:06 +0900

    [go/users] Set 30 seconds timeout when Golang container waits Postgres container to start.

--- go/users/scripts/wait_for_postgres.sh
+++ go/users/scripts/wait_for_postgres.sh
@@ -1,13 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 # Load the .env file.
-source ../.env
+source .env
 set -e
 
-until psql -c '\q';
-do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
+# Set the timeout in seconds.
+TIMEOUT=30
 
->&2 echo "Postgres is up"
+wait_for_postgres() {
+    echo "Waiting for Postgres to start..."
+    if timeout $TIMEOUT bash -c 'until psql -c "\q"; do sleep 2; done'; then
+        echo "Postgres is ready!"
+    else
+        echo "Timeout reached. Postgres did not start within $TIMEOUT seconds."
+        exit 1
+    fi
+}
+
+wait_for_postgres
