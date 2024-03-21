28004db8d7fb853460e2627cb0cda7a3644c19da
ryuji0123 <rokuda1011@gmail.com>
2024-03-21 22:13:08 +0900

    [iOS] Implement SideBar animation skeleton.

--- swift/Twitter-iOS/Twitter-iOS/ContentView.swift
+++ swift/Twitter-iOS/Twitter-iOS/ContentView.swift
@@ -7,14 +7,52 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
+  @State var horizontalOffset = LayoutConstant.mainOffset
+
+  private enum LayoutConstant {
+    static let screenWidth = UIScreen.main.bounds.width
+
+    static let sideBarOffset = screenWidth / 2 - 90
+    static let mainOffset = -screenWidth / 2
+    static let halfOffset = (sideBarOffset + mainOffset) / 2
+  }
+
   var body: some View {
-    TabView {
-      HomeView().tabItem { Image(systemName: "house") }
-      SearchView().tabItem { Image(systemName: "magnifyingglass") }
-      CommunitiesHomeView().tabItem { Image(systemName: "person.2") }
-      NotificationsView().tabItem { Image(systemName: "bell") }
-      MessagesView().tabItem { Image(systemName: "envelope") }
+    HStack {
+      Text("Hi")
+        .frame(width: LayoutConstant.screenWidth)
+
+      TabView {
+        HomeView().tabItem { Image(systemName: "house") }
+        SearchView().tabItem { Image(systemName: "magnifyingglass") }
+        CommunitiesHomeView().tabItem { Image(systemName: "person.2") }
+        NotificationsView().tabItem { Image(systemName: "bell") }
+        MessagesView().tabItem { Image(systemName: "envelope") }
+      }
+      .frame(width: LayoutConstant.screenWidth)
     }
+    .frame(width: 2*LayoutConstant.screenWidth)
+    .offset(x: horizontalOffset)
+    .gesture(DragGesture()
+      .onChanged { value in
+        withAnimation {
+          if value.translation.width > 0 && LayoutConstant.mainOffset <= horizontalOffset {
+            horizontalOffset = LayoutConstant.mainOffset + value.translation.width
+          } else if value.translation.width < 0 &&  horizontalOffset > LayoutConstant.mainOffset {
+            horizontalOffset = LayoutConstant.sideBarOffset + value.translation.width
+          }
+        }
+      }
+      .onEnded { value in
+        withAnimation {
+          if horizontalOffset >= LayoutConstant.halfOffset {
+            horizontalOffset = LayoutConstant.sideBarOffset
+          } else {
+            horizontalOffset = LayoutConstant.mainOffset
+          }
+        }
+      }
+    )
   }
 }
 
