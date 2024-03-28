8ee66f5861764f66e4467999be9fe5e53c718ee4
eightsuzuki <suzukieight8@gmail.com>
2024-03-29 05:05:43 +0900

    Align the CSS of Sidebar

--- twitter-clone-web/components/Sidebar.js
+++ twitter-clone-web/components/Sidebar.js
@@ -27,26 +27,22 @@ export default function Sidebar() {
       backgroundColor={"gray.100"}
     >
       <Box
-        className={`hoverEffect ${isHovered ? "bg-blue-100" : ""}`}
-        p={0}
-        px={{ base: 0, xl: 1 }}
-        _hover={{ bg: "gray.200" }}
-        borderRadius="full"
-      >
-        <Image
-          src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"
-          width={50}
-          height={50}
-        />
-      </Box>
-
-      
-      <Box
         mt={4}
         mb={2.5}
-        flexDirection={{ base: "column", xl: "colimn" }}
+        flexDirection={{ base: "column", xl: "row" }}
         alignItems="start"
       >
+        <Box
+          className={`hoverEffect ${isHovered ? "bg-blue-100" : ""}`}
+          _hover={{ bg: "gray.200" }}
+          borderRadius="full"
+        >
+          <Image
+            src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"
+            width="60px"
+            height="60px"
+          />
+        </Box>
         <SidebarMenuItem text="Home" Icon={HomeIcon} active />
         <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
         <SidebarMenuItem text="Notifications" Icon={BellIcon} />
@@ -58,43 +54,47 @@ export default function Sidebar() {
       </Box>
 
       {/* profile */}
+      <Button
+        bg="blue.400"
+        color="white"
+        rounded="full"
+        w="56"
+        h="12"
+        fontWeight="bold"
+        boxShadow="md"
+        _hover={{ filter: "brightness(95%)" }}
+        fontSize="lg"
+        display={{ base: "none", xl: "inline" }}
+      >
+        Tweet
+      </Button>
 
-      {/* <Flex alignItems="center">
-        <Button
-          bg="blue.400"
-          color="white"
-          rounded="full"
-          w="56"
-          h="12"
-          fontWeight="bold"
-          boxShadow="md"
-          _hover={{ filter: "brightness(95%)" }}
-          fontSize="lg"
-          display={{ base: "none", xl: "inline" }}
-        >
-          Tweet
-        </Button>
-
+      <Flex
+        display="flex"
+        alignItems="center"
+        mt={{ base: 0, xl: 2 }}
+        borderRadius="full"
+        _hover={{ bg: "gray.200" }}
+        p = {{ base: 0, xl: 3 }}
+      >
+        <Avatar
+          src="https://eightsuzuki.github.io/images/profile.png"
+          alt="user-img"
+          size={{ base: "sm", xl: "md" }}
+        />
         <Flex
-          className="hoverEffect"
-          color="gray.700"
-          alignItems="center"
-          justifyContent={{ base: "center", xl: "flex-start" }}
-          mt="auto"
+          direction="row"
+          display={{ base: "none", xl: "inline" }}
+          ml={2}
+          mr={2}
         >
-          <Avatar
-            src="https://eightsuzuki.github.io/images/profile.png"
-            alt="user-img"
-            size="sm"
-            mr="2"
-          />
-          <Flex direction="column" className="leading-5 hidden xl:inline">
-            <Text fontWeight="bold">sugiki8</Text>
-            <Text color="gray.500">@8Infu</Text>
-          </Flex>
-          <DotsHorizontalIcon className="h-5 ml-8 hidden xl:inline" />
+          <Text fontWeight="bold">sugiki8</Text>
+          <Text color="gray.500">@8Infu</Text>
         </Flex>
-      </Flex> */}
+        <Flex direction="row" display={{ base: "none", xl: "inline" }} mr={2}>
+          <DotsHorizontalIcon height="28px" />
+        </Flex>
+      </Flex>
     </Flex>
   );
 }
--- twitter-clone-web/components/SidebarMenuItem.js
+++ twitter-clone-web/components/SidebarMenuItem.js
@@ -11,10 +11,10 @@ export default function SidebarMenuItem({ text, Icon, active }) {
       _hover={{ bg: "gray.200" }}
 
       fontSize="lg"
-      direction={{ base: "column", lg: "row" }}
+      direction={{ base: "column", xl: "row" }}
     >
       <Icon as={Icon} height="28px" />
-      <Text fontWeight={active ? "bold" : "normal"} display={{ base: "none", lg:"inline",xl: "inline" }}>
+      <Text fontWeight={active ? "bold" : "normal"} display={{ base: "none",xl: "inline" }}>
         {text}
       </Text>
     </Flex>
