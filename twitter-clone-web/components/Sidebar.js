e925db92414774664a3968bed7dd5ab78262cf68
eightsuzuki <suzukieight8@gmail.com>
2024-03-29 02:49:38 +0900

    Add Profile and Other Button and Align the CSS

--- twitter-clone-web/components/Sidebar.js
+++ twitter-clone-web/components/Sidebar.js
@@ -1,5 +1,15 @@
-import { Box } from "@chakra-ui/react";
-import { HomeIcon } from "@heroicons/react/solid";
+import { Box, Button, Flex, Avatar, Icon, Text } from "@chakra-ui/react";
+import {
+  HomeIcon,
+  BellIcon,
+  BookmarkIcon,
+  ClipboardIcon,
+  DotsCircleHorizontalIcon,
+  DotsHorizontalIcon,
+  HashtagIcon,
+  InboxIcon,
+  UserIcon,
+} from "@heroicons/react/solid";
 import SidebarMenuItem from "./SidebarMenuItem";
 
 export default function Sidebar() {
@@ -11,11 +21,39 @@ export default function Sidebar() {
         alt="Twitter Logo"
         width="50"
         height="50"
-        borderRadius="full" // 丸みを持たせる
-        boxShadow="md" // 影をつける
-        backgroundColor={"blue.600"}
       />
-      <SidebarMenuItem text="Home" Icon={HomeIcon} active={true} />
+      <Box>
+        <SidebarMenuItem text="Home" Icon={HomeIcon} active />
+        <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
+        <SidebarMenuItem text="Notifications" Icon={BellIcon} />
+        <SidebarMenuItem text="Messages" Icon={InboxIcon} />
+        <SidebarMenuItem text="Bookmarks" Icon={BookmarkIcon} />
+        <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
+        <SidebarMenuItem text="Profile" Icon={UserIcon} />
+        <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon} />
+      </Box>
+
+      {/* profile */}
+
+      <Flex alignItems="center">
+        <Button colorScheme="blue" mr="4">
+          Tweet
+        </Button>
+
+        <Flex alignItems="center">
+          <Avatar
+            src="https://eightsuzuki.github.io/images/profile.png"
+            alt="user-img"
+            size="sm"
+            mr="2"
+          />
+          <Flex flexDirection="column">
+            <Text fontWeight="bold">suzuki8</Text>
+            <Text color="gray.500">@8Infu</Text>
+          </Flex>
+          <Icon as={DotsHorizontalIcon} boxSize={5} ml="2" />
+        </Flex>
+      </Flex>
     </Box>
   );
 }
--- twitter-clone-web/components/SidebarMenuItem.js
+++ twitter-clone-web/components/SidebarMenuItem.js
@@ -1,10 +1,25 @@
-import { Flex, Text, Icon } from "@chakra-ui/react";
+import { Flex, Icon, Text } from "@chakra-ui/react";
 
 export default function SidebarMenuItem({ text, Icon, active }) {
   return (
-    <Flex>
-      <Icon as={Icon} boxSize={7} mr="2" />
-      <Text>{text}</Text>
+    <Flex
+      className="hoverEffect"
+      alignItems="center"
+      color="gray.700"
+      justifyContent="center"
+      borderRadius="full"
+      width="52px"
+      height="52px"
+      padding="3"
+      _hover={{ bg: "gray.200" }}
+      xl={{ justifyContent: "flex-start" }}
+      fontSize="lg"
+      spaceX="3"
+    >
+      <Icon as={Icon} boxSize={7} />
+      <Text fontWeight={active ? "bold" : "normal"} display={{ base: "none", xl: "inline" }}>
+        {text}
+      </Text>
     </Flex>
   );
 }
