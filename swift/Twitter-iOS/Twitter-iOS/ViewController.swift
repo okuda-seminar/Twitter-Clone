a3b5ea7c7ef0a4ae0b64c0801b343d8c8835768c
ryuji0123 <rokuda1011@gmail.com>
2024-03-28 19:12:11 +0900

    [iOS] Replace ContentView (SwiftUI) with RootViewController (UIKit).

--- swift/Twitter-iOS/Twitter-iOS/ViewController.swift
+++ swift/Twitter-iOS/Twitter-iOS/ViewController.swift
@@ -1,9 +1,28 @@
 import UIKit
 
-class ViewController: UIViewController {
+class RootViewController: UIViewController {
+
+  private let helloLabel: UILabel = {
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.text = "Hello"
+    return label
+  }()
 
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    setUpSubviews()
+  }
+
+  private func setUpSubviews() {
+    view.backgroundColor = .white
+    view.addSubview(helloLabel)
+
+    NSLayoutConstraint.activate([
+      helloLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      helloLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+    ])
   }
 }
 
