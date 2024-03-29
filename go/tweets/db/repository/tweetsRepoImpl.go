cdcc04ac49737dca16c550e0d9260b5a6157dbaf
Ryuju0911 <pinebook0911@gmail.com>
2024-03-29 12:07:27 +0900

    [go/tweets] Format comments.

--- go/tweets/db/repository/tweetsRepoImpl.go
+++ go/tweets/db/repository/tweetsRepoImpl.go
@@ -17,7 +17,6 @@ func NewTweetsRepoImpl(db *sql.DB) TweetsRepo {
 
 // CreateTweet creates a new tweet entry and inserts it into 'tweets' table.
 func (r *tweetsRepoImpl) CreateTweet(ctx context.Context, user_id int, text string) (*Tweet, error) {
-	// TODO: https://github.com/okuda-seminar/Twitter-Clone/issues/69
-	// [go/tweets] Implement CreateTweet API logic
+	// TODO: #69 - [go/tweets] Implement CreateTweet API logic.
 	return nil, nil
 }
