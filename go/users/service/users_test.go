09f68da3718bfe38acc7855bc7b4ba0eb322e006
Ryuju0911 <pinebook0911@gmail.com>
2024-05-02 20:42:35 +0900

    [go/users] Add tests for GetFollowers and GetFollowings.

--- go/users/service/users_test.go
+++ go/users/service/users_test.go
@@ -26,7 +26,7 @@ func (dbMock *DBMock) close() {
 }
 
 func (dbMock *DBMock) insertUser(user *repository.User) {
-	rows := sqlmock.NewRows([]string{"id", "username", "account_name", "bio", "created_at", "updated_at"}).
+	rows := sqlmock.NewRows([]string{"id", "username", "display_name", "bio", "created_at", "updated_at"}).
 		AddRow(user.ID, user.Username, user.DisplayName, user.Bio, user.CreatedAt, user.UpdatedAt)
 
 	query := "SELECT * FROM users WHERE id = $1"
@@ -83,7 +83,7 @@ func TestUpdateUsername(t *testing.T) {
 	}
 	defer dbMock.close()
 
-	query := "UPDATE users SET username = $1 where user_id = $2"
+	query := "UPDATE users SET username = $1 where id = $2"
 
 	dbMock.mock.ExpectExec(regexp.QuoteMeta(query)).
 		WithArgs("updated", 1).
@@ -130,7 +130,7 @@ func TestUpdateBio(t *testing.T) {
 	}
 	defer dbMock.close()
 
-	query := "UPDATE users SET bio = $1 where user_id = $2"
+	query := "UPDATE users SET bio = $1 where id = $2"
 	dbMock.mock.ExpectExec(regexp.QuoteMeta(query)).
 		WithArgs("updated", 1).
 		WillReturnResult(sqlmock.NewResult(1, 1))
@@ -161,5 +161,77 @@ func TestUpdateBio(t *testing.T) {
 				t.Errorf("expected nil, but got err: %v", err)
 			}
 		}
+	}
+}
+
+func TestGetFollowers(t *testing.T) {
+	service, dbMock, err := setup()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer dbMock.close()
+
+	rows := sqlmock.NewRows([]string{
+		"id", "username", "display_name", "bio", "created_at", "updated_at",
+		"follower_id", "followee_id",
+	}).
+		// User with ID "2" follows user with ID "1".
+		AddRow(2, "second", "second", "", time.Now(), time.Now(), 2, 1).
+		// User with ID "3" follows user with ID "1".
+		AddRow(3, "third", "third", "", time.Now(), time.Now(), 3, 1)
+
+	query := `
+SELECT * FROM users
+JOIN followships ON users.id = followships.follower_id
+WHERE followships.followee_id = $1
+`
+	dbMock.mock.ExpectQuery(regexp.QuoteMeta(query)).
+		WithArgs(1).
+		WillReturnRows(rows)
+
+	p := &users.GetFollowersPayload{ID: 1}
+	followers, err := service.GetFollowers(context.Background(), p)
+	if err != nil {
+		t.Errorf("cannot get followers: %s", err)
+	}
+
+	if len(followers) != 2 {
+		t.Errorf("the number of followers was %d, not 2", len(followers))
+	}
+}
+
+func TestGetFollowings(t *testing.T) {
+	service, dbMock, err := setup()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer dbMock.close()
+
+	rows := sqlmock.NewRows([]string{
+		"id", "username", "display_name", "bio", "created_at", "updated_at",
+		"follower_id", "followee_id",
+	}).
+		// User with ID "1" follows user with ID "2".
+		AddRow(2, "second", "second", "", time.Now(), time.Now(), 1, 2).
+		// User with ID "1" follows user with ID "3".
+		AddRow(3, "third", "third", "", time.Now(), time.Now(), 1, 3)
+
+	query := `
+SELECT * FROM users
+JOIN followships ON users.id = followships.followee_id
+WHERE followships.follower_id = $1
+`
+	dbMock.mock.ExpectQuery(regexp.QuoteMeta(query)).
+		WithArgs(1).
+		WillReturnRows(rows)
+
+	p := &users.GetFollowingsPayload{ID: 1}
+	followers, err := service.GetFollowings(context.Background(), p)
+	if err != nil {
+		t.Errorf("cannot get followings: %s", err)
+	}
+
+	if len(followers) != 2 {
+		t.Errorf("the number of followings was %d, not 2", len(followers))
 	}
 }
