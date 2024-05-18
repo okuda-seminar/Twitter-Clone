05619ace8335ef7333a8dc460ff82dcb6c107a2e
TakayaShirai <s78taka03@gmail.com>
2024-05-18 15:29:24 +0900

    [iOS] Replace custom home header view with Apple's navigation bar items

---
+++ swift/Twitter-iOS/Twitter-iOS/Features/Home/HomeTabSelectionView.swift
@@ -0,0 +1,55 @@
+import SwiftUI
+import UIKit
+
+class HomeTabSelectionView: UIView {
+
+  private enum LocalizedString {
+    static let forYouButtonTitle = String(localized: "For You")
+    static let followingButtonTitle = String(localized: "Following")
+  }
+
+  public let forYouButton: HomeHeaderButton = {
+    let button = HomeHeaderButton()
+    button.setTitle(LocalizedString.forYouButtonTitle, for: .normal)
+    button.isSelected = true
+    button.translatesAutoresizingMaskIntoConstraints = false
+    return button
+  }()
+
+  public let followingButton: HomeHeaderButton = {
+    let button = HomeHeaderButton()
+    button.setTitle(LocalizedString.followingButtonTitle, for: .normal)
+    button.isSelected = false
+    button.translatesAutoresizingMaskIntoConstraints = false
+    return button
+  }()
+
+  public lazy var allButtons: [HomeHeaderButton] = {
+    return [forYouButton, followingButton]
+  }()
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setUpSubviews()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  private func setUpSubviews() {
+    let stackView = UIStackView(arrangedSubviews: [forYouButton, followingButton])
+    stackView.axis = .horizontal
+    stackView.distribution = .fillEqually
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+
+    addSubview(stackView)
+
+    NSLayoutConstraint.activate([
+      stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+      stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+      stackView.topAnchor.constraint(equalTo: topAnchor),
+      stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+    ])
+  }
+}
