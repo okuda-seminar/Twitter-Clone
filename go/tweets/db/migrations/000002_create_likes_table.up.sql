8f64cabc3ff1c4ed4e51059a4eaf06449294c157
ryuju0911 <pinebook0911@gmail.com>
2024-05-19 20:50:35 +0900

    [go/tweets] Fix wrong syntax of SQL.

--- go/tweets/db/migrations/000002_create_likes_table.up.sql
+++ go/tweets/db/migrations/000002_create_likes_table.up.sql
@@ -2,5 +2,5 @@ CREATE TABLE likes (
     tweet_id UUID NOT NULL,
     user_id UUID NOT NULL,
     PRIMARY KEY (tweet_id, user_id),
-    FOREIGN KEY (tweet_id) tweets(id)
+    FOREIGN KEY (tweet_id) REFERENCES tweets(id)
 );
