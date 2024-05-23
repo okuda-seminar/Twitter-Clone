5d66bf3ebafb481c580c7c168904ce199064764b
kasturada1 <kazuki1.katsurada@gmail.com>
2024-05-23 15:47:13 +0900

    [go/notification] Introduce goa

---
+++ go/notification/design/design.go
@@ -0,0 +1,19 @@
+package design
+
+import . "goa.design/goa/v3/dsl"
+
+var _ = API("notification", func() {
+	Title("Notification Service")
+	Description("Service for managing notification")
+	Server("notification", func() {
+		Host("localhost", func() {
+			URI("http://0.0.0.0:80/api/notification")
+		})
+	})
+})
+
+var _ = Service("notification", func() {
+	Description("The notification service performs operations on notification information.")
+
+	Files("/swagger.json", "./gen/http/openapi.json")
+})
