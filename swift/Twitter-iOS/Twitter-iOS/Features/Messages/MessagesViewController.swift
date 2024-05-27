d4547daed5c4f0d11eaf324ac81fe19f23d03e2f
TakayaShirai <s78taka03@gmail.com>
2024-05-27 10:51:03 +0900

    [iOS] Add another new message entry point button to Messages.

--- swift/Twitter-iOS/Twitter-iOS/Features/Messages/MessagesViewController.swift
+++ swift/Twitter-iOS/Twitter-iOS/Features/Messages/MessagesViewController.swift
@@ -4,13 +4,42 @@ import UIKit
 class MessagesViewController: ViewControllerWithUserIconButton {
 
   private enum LayoutConstant {
-    static let profileIconButtonSize = 28.0
-    static let edgePadding = 16.0
+    static let profileIconButtonSize: CGFloat = 28.0
+    static let edgePadding: CGFloat = 16.0
+    static let headlineViewVerticalEdgePadding: CGFloat = 34
+    static let headlineViewSpacing: CGFloat = 10
+    static let writeMessageButtonVerticalEdgePadding: CGFloat = 25
+    static let headlineFontSize: CGFloat = 29
+    static let subHeadlineFontSize: CGFloat = 15
   }
 
   private enum LocalizedString {
     static let title = String(localized: "Messages")
+    static let headlineText = String(localized: "Welcome to your inbox!")
+    static let subHeadlineText = String(
+      localized:
+        "Drop a line, share posts and more with private conversations between you and others on Twitter."
+    )
   }
+
+  private let headlineLabel: UILabel = {
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.text = LocalizedString.headlineText
+    label.numberOfLines = 0
+    label.font = UIFont.systemFont(ofSize: LayoutConstant.headlineFontSize, weight: .heavy)
+    return label
+  }()
+
+  private let subHeadlineLabel: UILabel = {
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.text = LocalizedString.subHeadlineText
+    label.textColor = .gray
+    label.numberOfLines = 0
+    label.font = UIFont.systemFont(ofSize: LayoutConstant.subHeadlineFontSize)
+    return label
+  }()
 
   private lazy var profileIconButton: UIBarButtonItem = {
     let button = UIBarButtonItem(
@@ -28,8 +57,17 @@ class MessagesViewController: ViewControllerWithUserIconButton {
     return button
   }()
 
+  private lazy var writeMessageButtonController: NewMessageEntrypointButtonController = {
+    let viewController = NewMessageEntrypointButtonController(buttonType: .writeMessageButton)
+    viewController.view.translatesAutoresizingMaskIntoConstraints = false
+    addChild(viewController)
+    viewController.didMove(toParent: self)
+    return viewController
+  }()
+
   private lazy var newMessageEntryPointButtonController: NewMessageEntrypointButtonController = {
-    let viewController = NewMessageEntrypointButtonController()
+    let viewController = NewMessageEntrypointButtonController(
+      buttonType: .newMessageEntrypointButton)
     viewController.view.translatesAutoresizingMaskIntoConstraints = false
     addChild(viewController)
     viewController.didMove(toParent: self)
@@ -42,12 +80,32 @@ class MessagesViewController: ViewControllerWithUserIconButton {
   }
 
   private func setUpSubviews() {
+    let headlineView = UIStackView(arrangedSubviews: [headlineLabel, subHeadlineLabel])
+    headlineView.axis = .vertical
+    headlineView.spacing = LayoutConstant.headlineViewSpacing
+    headlineView.translatesAutoresizingMaskIntoConstraints = false
+
+    view.addSubview(headlineView)
+    view.addSubview(writeMessageButtonController.view)
     view.addSubview(newMessageEntryPointButtonController.view)
 
     view.backgroundColor = .systemBackground
 
     let layoutGuide = view.safeAreaLayoutGuide
     NSLayoutConstraint.activate([
+      headlineView.topAnchor.constraint(
+        equalTo: layoutGuide.topAnchor, constant: LayoutConstant.headlineViewVerticalEdgePadding),
+      headlineView.leadingAnchor.constraint(
+        equalTo: layoutGuide.leadingAnchor, constant: LayoutConstant.edgePadding),
+      headlineView.trailingAnchor.constraint(
+        equalTo: layoutGuide.trailingAnchor, constant: -LayoutConstant.edgePadding),
+
+      writeMessageButtonController.view.topAnchor.constraint(
+        equalTo: headlineView.bottomAnchor,
+        constant: LayoutConstant.writeMessageButtonVerticalEdgePadding),
+      writeMessageButtonController.view.leadingAnchor.constraint(
+        equalTo: layoutGuide.leadingAnchor, constant: LayoutConstant.edgePadding),
+
       newMessageEntryPointButtonController.view.bottomAnchor.constraint(
         equalTo: layoutGuide.bottomAnchor, constant: -LayoutConstant.edgePadding),
       newMessageEntryPointButtonController.view.trailingAnchor.constraint(
--- swift/Twitter-iOS/Twitter-iOS/Features/Messages/NewMessageEntrypointButtonController.swift
+++ swift/Twitter-iOS/Twitter-iOS/Features/Messages/NewMessageEntrypointButtonController.swift
@@ -1,10 +1,33 @@
 import UIKit
 
 final class NewMessageEntrypointButtonController: UIViewController {
+  public enum NewMessageButtonType: Int {
+    case newMessageEntrypointButton
+    case writeMessageButton
+  }
+
+  public let buttonType: NewMessageButtonType
+
   override func viewDidLoad() {
     super.viewDidLoad()
+    setUpSubviews()
+  }
 
-    let button = NewMessageEntrypointButton()
+  init(buttonType: NewMessageButtonType) {
+    self.buttonType = buttonType
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  private func setUpSubviews() {
+    let button =
+      switch buttonType {
+      case .newMessageEntrypointButton: NewMessageEntrypointButton()
+      case .writeMessageButton: WriteMessageButton()
+      }
     button.addAction(
       .init { _ in
         let newMessageEditViewController = NewMessageEditViewController()
@@ -39,6 +62,45 @@ final class NewMessageEntrypointButton: UIButton {
     NSLayoutConstraint.activate([
       widthAnchor.constraint(equalToConstant: LayoutConstant.buttonSize),
       heightAnchor.constraint(equalToConstant: LayoutConstant.buttonSize),
+    ])
+  }
+}
+
+final class WriteMessageButton: UIButton {
+  private enum LayoutConstant {
+    static let buttonWidth: CGFloat = 200.0
+    static let buttonHeight: CGFloat = 55.0
+  }
+
+  private enum LocalizedString {
+    static let buttonTitle = String(localized: "Write a message")
+  }
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setupButton()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  private func setupButton() {
+    let titleWithUnderLine = NSAttributedString(
+      string: LocalizedString.buttonTitle,
+      attributes: [
+        .underlineStyle: NSUnderlineStyle.single.rawValue, .underlineColor: UIColor.white,
+      ])
+    setAttributedTitle(titleWithUnderLine, for: .normal)
+    titleLabel?.textColor = .white
+
+    layer.cornerRadius = LayoutConstant.buttonHeight / 2
+    backgroundColor = .brandedBlue
+    translatesAutoresizingMaskIntoConstraints = false
+
+    NSLayoutConstraint.activate([
+      widthAnchor.constraint(equalToConstant: LayoutConstant.buttonWidth),
+      heightAnchor.constraint(equalToConstant: LayoutConstant.buttonHeight),
     ])
   }
 }
