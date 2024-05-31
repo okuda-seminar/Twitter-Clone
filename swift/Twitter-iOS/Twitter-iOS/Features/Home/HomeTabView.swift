48d101b67ac2868db4dadd3f73e136119077d224
ryuji0123 <rokuda1011@gmail.com>
2024-05-31 22:24:49 +0900

    [iOS] Show an alert when the mute action is selected in Home.

--- swift/Twitter-iOS/Twitter-iOS/Features/Home/HomeTabView.swift
+++ swift/Twitter-iOS/Twitter-iOS/Features/Home/HomeTabView.swift
@@ -9,11 +9,27 @@ struct HomeTabView: View {
     return models
   }()
 
-  private enum LocalizedLongTapActionString {
+  @State private var isMuteAlertPresented = false
+
+  private let userName = "@mockUserName"
+
+  private enum LocalizedString {
+    static let alertDismissalText = String(localized: "Cancel")
+    static let alertAcceptanceText = String(localized: "Yes, I'm sure")
+
     static let unfollow = String(localized: "Unfollow")
     static let addOrRemoveFromLists = String(localized: "Add/remove from Lists")
     static let block = String(localized: "Block")
+
     static let mute = String(localized: "Mute")
+    static let muteAlertTitle = String(localized: "Mute %@")
+    static let muteAlertMessage = String(
+      localized: """
+            You won't see posts from %@ or get notifications about them. They won't know
+            they've been muted.
+        """)
+    static let muteBannerTitle = String(localized: "%@ has benn muted.")
+
     static let reportPost = String(localized: "Report post")
   }
 
@@ -29,17 +45,9 @@ struct HomeTabView: View {
             ContextMenu(menuItems: {
               Button(
                 action: {
-                  let userName = "@mockUserName"
-                  UserService.sharedInstance.unfollowUser(userName) { success in
-                    if success {
-                      let bannerController = BannerController(
-                        message: "\(userName) has been unfollowed")
-                      bannerController.show(on: MainRootViewController.sharedInstance)
-                    }
-                  }
                 },
                 label: {
-                  Text(LocalizedLongTapActionString.unfollow)
+                  Text(LocalizedString.unfollow)
                   Image(systemName: "person.fill.xmark")
                 })
               Button(
@@ -47,15 +55,15 @@ struct HomeTabView: View {
 
                 },
                 label: {
-                  Text(LocalizedLongTapActionString.addOrRemoveFromLists)
+                  Text(LocalizedString.addOrRemoveFromLists)
                   Image(systemName: "list.clipboard")
                 })
               Button(
                 action: {
-
+                  isMuteAlertPresented.toggle()
                 },
                 label: {
-                  Text(LocalizedLongTapActionString.mute)
+                  Text(LocalizedString.mute)
                   Image(systemName: "speaker.slash")
                 })
               Button(
@@ -63,7 +71,7 @@ struct HomeTabView: View {
 
                 },
                 label: {
-                  Text(LocalizedLongTapActionString.block)
+                  Text(LocalizedString.block)
                   Image(systemName: "xmark.circle")
                 })
               Button(
@@ -71,11 +79,37 @@ struct HomeTabView: View {
 
                 },
                 label: {
-                  Text(LocalizedLongTapActionString.reportPost)
+                  Text(LocalizedString.reportPost)
                   Image(systemName: "flag")
                 })
             }))
         }
+      }
+      .alert(
+        String(format: LocalizedString.muteAlertTitle, userName), isPresented: $isMuteAlertPresented
+      ) {
+        Button(
+          role: .cancel, action: {},
+          label: {
+            Text(String(format: LocalizedString.alertDismissalText))
+          })
+
+        Button(
+          role: .destructive,
+          action: {
+            UserService.sharedInstance.muteUser(userName) { success in
+              if success {
+                let bannerController = BannerController(
+                  message: String(format: LocalizedString.muteBannerTitle, userName))
+                bannerController.show(on: MainRootViewController.sharedInstance)
+              }
+            }
+          },
+          label: {
+            Text(String(format: LocalizedString.alertAcceptanceText))
+          })
+      } message: {
+        Text(String(format: LocalizedString.muteAlertMessage, userName))
       }
     }
   }
