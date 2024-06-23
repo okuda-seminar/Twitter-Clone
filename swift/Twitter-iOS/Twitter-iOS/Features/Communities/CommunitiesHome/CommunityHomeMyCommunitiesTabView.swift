9c9676b84b9e072d16061f4d96a10df0012f724b
ryuji0123 <rokuda1011@gmail.com>
2024-06-23 21:46:24 +0900

    [iOS] Add tabs to Community home.

---
+++ swift/Twitter-iOS/Twitter-iOS/Features/Communities/CommunitiesHome/CommunityHomeMyCommunitiesTabView.swift
@@ -0,0 +1,68 @@
+import SwiftUI
+
+struct CommunityHomeMyCommunitiesTabView: View {
+
+  @Binding public var showMoreCommunities: Bool
+
+  private enum LayoutConstant {
+    static let edgePadding = 16.0
+  }
+
+  private enum LocalizedString {
+    static let headlineLabelText = String(localized: "Discover new Communities")
+    static let seeLessOften = String(localized: "See less often")
+    static let showMoreText = String(localized: "Show more")
+  }
+
+  private let communities: [CommunityModel] = {
+    var communities: [CommunityModel] = []
+    for _ in 0..<4 {
+      communities.append(createFakeCommunityModel())
+    }
+    return communities
+  }()
+
+  var body: some View {
+    VStack {
+      HStack {
+        Text(LocalizedString.headlineLabelText)
+        Spacer()
+        Menu {
+          Button(
+            action: {
+            },
+            label: {
+              Label(LocalizedString.seeLessOften, systemImage: "face.dashed")
+            })
+        } label: {
+          Image(systemName: "ellipsis")
+        }
+        .foregroundStyle(Color(uiColor: .brandedLightGrayText))
+      }
+
+      ForEach(communities) { community in
+        CommunityCellView(community: community)
+      }
+
+      HStack {
+        Button(
+          action: {
+            showMoreCommunities = true
+          },
+          label: {
+            Text(LocalizedString.showMoreText)
+          })
+        Spacer()
+      }
+
+      Spacer()
+    }
+    .padding(.top, LayoutConstant.edgePadding)
+    .padding(.leading, LayoutConstant.edgePadding)
+    .padding(.trailing, LayoutConstant.edgePadding)
+  }
+}
+
+#Preview {
+  CommunityHomeMyCommunitiesTabView(showMoreCommunities: .constant(false))
+}
