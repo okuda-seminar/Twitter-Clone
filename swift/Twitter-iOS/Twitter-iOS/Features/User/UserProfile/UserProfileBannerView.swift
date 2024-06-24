c2ee450409d6a6e6753a26973d4c5cc3020ecc10
TakayaShirai <s78taka03@gmail.com>
2024-06-25 00:28:06 +0900

    [iOS] Add Button with dummy Menu to User Profile.

--- swift/Twitter-iOS/Twitter-iOS/Features/User/UserProfile/UserProfileBannerView.swift
+++ swift/Twitter-iOS/Twitter-iOS/Features/User/UserProfile/UserProfileBannerView.swift
@@ -32,6 +32,10 @@ struct UserProfileBannerView: View {
             .opacity(blurViewOpacity)
 
           VStack(spacing: 5) {
+            // TODO: https://github.com/okuda-seminar/Twitter-Clone/issues/329
+            // - Adjust Layout for UserProfileButtonsView, Username, and Post Count in UserProfileBannerView.swift.
+            UserProfileButtonsView()
+
             Text("userName")
               .fontWeight(.bold)
               .foregroundColor(.white)
@@ -68,6 +72,154 @@ struct UserProfileBlurView: UIViewRepresentable {
   }
 
   func updateUIView(_ uiView: UIVisualEffectView, context: Context) {}
+}
+
+struct UserProfileButtonsView: View {
+
+  private enum LayoutConstant {
+    static let backgroundCircleSize: CGFloat = 30.0
+    static let backgroundCircleOpacity: CGFloat = 0.5
+  }
+
+  private enum LocalizedString {
+    static let turnOffReportsText = String(localized: "Turn off reports")
+    static let viewTopicsText = String(localized: "View Topics")
+    static let addOrRemoveListsText = String(localized: "Add/remove from Lists")
+    static let viewListsText = String(localized: "View Lists")
+    static let shareText = String(localized: "Share @userName")
+    static let muteText = String(localized: "Mute @userName")
+    static let blockText = String(localized: "Block @userName")
+    static let reportText = String(localized: "Report @userName")
+  }
+
+  var body: some View {
+    HStack {
+      Button {
+        // TODO: https://github.com/okuda-seminar/Twitter-Clone/issues/321
+        // - Implement View Transition for Back Button in UserProfileBannerView.swift.
+      } label: {
+        Image(systemName: "arrow.left")
+          .background(
+            Circle()
+              .fill(Color.black)
+              .opacity(LayoutConstant.backgroundCircleOpacity)
+              .frame(
+                width: LayoutConstant.backgroundCircleSize,
+                height: LayoutConstant.backgroundCircleSize)
+          )
+          .foregroundColor(.white)
+      }
+      .padding(.leading)
+
+      Spacer()
+
+      Button {
+        // TODO: https://github.com/okuda-seminar/Twitter-Clone/issues/322
+        // - Implement View Transition for Search Button in UserProfileBannerView.swift.
+      } label: {
+        Image(systemName: "magnifyingglass")
+          .background(
+            Circle()
+              .fill(Color.black)
+              .opacity(LayoutConstant.backgroundCircleOpacity)
+              .frame(
+                width: LayoutConstant.backgroundCircleSize,
+                height: LayoutConstant.backgroundCircleSize)
+          )
+          .foregroundColor(.white)
+      }
+
+      Menu {
+        // TODO: https://github.com/okuda-seminar/Twitter-Clone/issues/320
+        // - Implement Appropriate Functionality for Each Menu Item in UserProfileBannerView.swift.
+        Button {
+
+        } label: {
+          HStack {
+            Text(LocalizedString.turnOffReportsText)
+            Image(systemName: "square.slash")
+          }
+        }
+
+        Button {
+
+        } label: {
+          HStack {
+            Text(LocalizedString.viewTopicsText)
+            Image(systemName: "text.bubble")
+          }
+        }
+
+        Button {
+
+        } label: {
+          HStack {
+            Text(LocalizedString.addOrRemoveListsText)
+            Image(systemName: "note.text.badge.plus")
+          }
+        }
+
+        Button {
+
+        } label: {
+          HStack {
+            Text(LocalizedString.viewListsText)
+            Image(systemName: "note.text")
+          }
+        }
+
+        Button {
+
+        } label: {
+          HStack {
+            Text(LocalizedString.shareText)
+            Image(systemName: "tray.and.arrow.up")
+          }
+        }
+
+        Button {
+
+        } label: {
+          HStack {
+            Text(LocalizedString.muteText)
+            Image(systemName: "speaker.slash")
+          }
+        }
+
+        Button {
+
+        } label: {
+          HStack {
+            Text(LocalizedString.blockText)
+            Image(systemName: "slash.circle")
+          }
+        }
+
+        Button {
+
+        } label: {
+          HStack {
+            Text(LocalizedString.reportText)
+            Image(systemName: "flag")
+          }
+        }
+
+      } label: {
+        Image(systemName: "ellipsis")
+          .background(
+            Circle()
+              .fill(Color.black)
+              .opacity(LayoutConstant.backgroundCircleOpacity)
+              .frame(
+                width: LayoutConstant.backgroundCircleSize,
+                height: LayoutConstant.backgroundCircleSize)
+          )
+          .foregroundColor(.white)
+      }
+      .padding(.horizontal)
+    }
+    .padding()
+  }
 }
 
 #Preview {
