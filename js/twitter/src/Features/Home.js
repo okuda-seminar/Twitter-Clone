e4f5ca843f7f04159ba719b78aa41891ebffad76
ryuji0123 <rokuda1011@gmail.com>
2024-07-09 21:19:07 +0900

    [JS] Replace Home Tab with ChakuraUI Tab components.

--- js/twitter/src/Features/Home.js
+++ js/twitter/src/Features/Home.js
@@ -1,61 +1,31 @@
 import "./Home.css";
-import { useState } from "react";
+import {
+  Tab,
+  Tabs,
+  TabIndicator,
+  TabList,
+  TabPanel,
+  TabPanels,
+} from "@chakra-ui/react";
 
 function Home() {
   return (
-    <div className="App">
-      <TabBar>
-        <Tab label="For You">
-          <div>Content for For You</div>
-        </Tab>
-        <Tab label="Following">
-          <div>Content for Follwing</div>
-        </Tab>
-      </TabBar>
-    </div>
+    <Tabs position="relative" variant="unstyled">
+      <TabList>
+        <Tab>For You</Tab>
+        <Tab>Following</Tab>
+      </TabList>
+      <TabIndicator mt="-1.5px" height="2px" bg="blue.500" borderRadius="1px" />
+      <TabPanels>
+        <TabPanel>
+          <p>one!</p>
+        </TabPanel>
+        <TabPanel>
+          <p>two!</p>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
   );
 }
-
-const TabBar = ({ children }) => {
-  const [activeTab, setActiveTab] = useState(children[0].props.label);
-
-  const onClickTabItem = (tab) => {
-    setActiveTab(tab);
-  };
-
-  return (
-    <div className="tab-bar">
-      <ol className="tab-list">
-        {children.map((child) => {
-          const { label } = child.props;
-
-          return (
-            <li
-              key={label}
-              className={
-                label === activeTab
-                  ? "tab-list-item tab-list-active"
-                  : "tab-list-item"
-              }
-              onClick={() => onClickTabItem(label)}
-            >
-              {label}
-            </li>
-          );
-        })}
-      </ol>
-      <div className="tab-content">
-        {children.map((child) => {
-          if (child.props.label !== activeTab) return undefined;
-          return child.props.children;
-        })}
-      </div>
-    </div>
-  );
-};
-
-const Tab = ({ label, children }) => {
-  return <div label={label}>{children}</div>;
-};
 
 export default Home;
