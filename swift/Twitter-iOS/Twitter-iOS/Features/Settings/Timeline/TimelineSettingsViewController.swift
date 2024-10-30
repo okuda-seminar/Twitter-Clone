9179d0d3ae56c046d58e529e4fea3e730fd1e025
TakayaShirai <s78taka03@gmail.com>
2024-10-30 01:27:06 -0700

    [iOS] Polish UI of TimelineSettingsViewController.

--- swift/Twitter-iOS/Twitter-iOS/Features/Settings/Timeline/TimelineSettingsViewController.swift
+++ swift/Twitter-iOS/Twitter-iOS/Features/Settings/Timeline/TimelineSettingsViewController.swift
@@ -1,57 +1,142 @@
+import SwiftUI
 import UIKit
 
-// TODO: https://github.com/okuda-seminar/Twitter-Clone/issues/466
-// - Polish UI of TimelineSettingsViewController.
 class TimelineSettingsViewController: UIViewController {
 
-  private enum LayoutConstant {
-    static let edgeHorizontalPadding = 16.0
-    static let headerViewHeight = 48.0
+  // MARK: - Private Props
+
+  private enum LocalizedString {
+    static let navigationBarTitle = String(localized: "Timeline")
   }
 
-  private let headerView: TimelineSettingsHeaderView = {
-    let view = TimelineSettingsHeaderView()
-    view.translatesAutoresizingMaskIntoConstraints = false
-    return view
+  private lazy var hostingController: UIHostingController = {
+    let controller = UIHostingController(rootView: TimelineSettingsView(delegate: self))
+    controller.view.translatesAutoresizingMaskIntoConstraints = false
+    addChild(controller)
+    controller.didMove(toParent: self)
+    return controller
   }()
 
-  private let bodyView: TimelineSettingsBodyView = {
-    let view = TimelineSettingsBodyView()
-    view.translatesAutoresizingMaskIntoConstraints = false
-    return view
+  private let titleLabel: UILabel = {
+    let label = UILabel()
+    label.text = LocalizedString.navigationBarTitle
+    label.font = .preferredFont(forTextStyle: .headline)
+    return label
   }()
+
+  private let subtitleLabel: UILabel = {
+    let label = UILabel()
+    // TODO: https://github.com/okuda-seminar/Twitter-Clone/issues/464
+    // - Enable Real Username Display in subtitleLabel in Settings View Controllers.
+
+    /// This is a placeholder username that does not need to be wrapped in LocalizedString.
+    /// It should be replaced with the real username variable in the future.
+    label.text = "@userName"
+    label.font = .preferredFont(forTextStyle: .footnote)
+    label.textColor = .gray
+    return label
+  }()
+
+  // MARK: - View Lifecycle
 
   override func viewDidLoad() {
     super.viewDidLoad()
     setUpSubviews()
   }
 
-  private func setUpSubviews() {
-    view.addSubview(headerView)
-    view.addSubview(bodyView)
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    setUpNavigation()
+  }
 
-    view.backgroundColor = .systemBackground
+  // MARK: - Private API
+
+  private func setUpSubviews() {
+    view.addSubview(hostingController.view)
 
     let layoutGuide = view.safeAreaLayoutGuide
     NSLayoutConstraint.activate([
-      headerView.leadingAnchor.constraint(
-        equalTo: layoutGuide.leadingAnchor, constant: LayoutConstant.edgeHorizontalPadding),
-      headerView.trailingAnchor.constraint(
-        equalTo: layoutGuide.trailingAnchor, constant: -LayoutConstant.edgeHorizontalPadding),
-      headerView.topAnchor.constraint(equalTo: layoutGuide.topAnchor),
-      headerView.heightAnchor.constraint(equalToConstant: LayoutConstant.headerViewHeight),
-
-      bodyView.leadingAnchor.constraint(
-        equalTo: layoutGuide.leadingAnchor, constant: LayoutConstant.edgeHorizontalPadding),
-      bodyView.trailingAnchor.constraint(
-        equalTo: layoutGuide.trailingAnchor, constant: -LayoutConstant.edgeHorizontalPadding),
-      bodyView.topAnchor.constraint(equalTo: headerView.bottomAnchor),
-      bodyView.bottomAnchor.constraint(equalTo: layoutGuide.bottomAnchor),
+      hostingController.view.leadingAnchor.constraint(equalTo: layoutGuide.leadingAnchor),
+      hostingController.view.trailingAnchor.constraint(equalTo: layoutGuide.trailingAnchor),
+      hostingController.view.topAnchor.constraint(equalTo: layoutGuide.topAnchor),
+      hostingController.view.bottomAnchor.constraint(equalTo: layoutGuide.bottomAnchor),
     ])
-
-    headerView.dismissButton.addAction(
-      .init { [weak self] _ in
-        self?.dismiss(animated: true)
-      }, for: .touchUpInside)
   }
+
+  private func setUpNavigation() {
+    let titleStackView = UIStackView(arrangedSubviews: [titleLabel, subtitleLabel])
+    titleStackView.axis = .vertical
+    titleStackView.alignment = .center
+
+    navigationItem.backButtonDisplayMode = .minimal
+    navigationItem.titleView = titleStackView
+    navigationItem.leftBarButtonItems = []
+    navigationController?.navigationBar.backgroundColor = .systemBackground
+  }
+}
+
+// MARK: - Delegate
+
+extension TimelineSettingsViewController: TimelineSettingsViewDelegate {
+  func navigationButtonDidReceiveTap() {
+    navigationController?.pushViewController(
+      TimelineSettingsHomeScreenTabsViewController(), animated: true)
+  }
+}
+
+// MARK: - View
+
+struct TimelineSettingsView: View {
+  public weak var delegate: TimelineSettingsViewDelegate?
+
+  private enum LocalizedString {
+    static let navigationButtonTitleText = String(localized: "Home screen tabs")
+    static let navigationButtonDescriptionText = String(
+      localized: "Reorder your list and community tabs on the home screen")
+
+  }
+
+  var body: some View {
+    VStack(alignment: .leading) {
+      NavigationButton()
+      NavigationButtonDescriptionText()
+
+      Spacer()
+    }
+    .padding(.horizontal)
+  }
+
+  @ViewBuilder
+  private func NavigationButton() -> some View {
+    Button {
+      delegate?.navigationButtonDidReceiveTap()
+    } label: {
+      HStack {
+        Text(LocalizedString.navigationButtonTitleText)
+          .fontWeight(.medium)
+          .foregroundStyle(.black)
+
+        Spacer()
+
+        Image(systemName: "chevron.right")
+          .foregroundStyle(.gray)
+      }
+      .padding(.vertical)
+    }
+  }
+
+  @ViewBuilder
+  private func NavigationButtonDescriptionText() -> some View {
+    Text(LocalizedString.navigationButtonDescriptionText)
+      .font(.body)
+      .foregroundStyle(.gray)
+  }
+}
+
+protocol TimelineSettingsViewDelegate: AnyObject {
+  func navigationButtonDidReceiveTap()
+}
+
+#Preview {
+  TimelineSettingsView()
 }
