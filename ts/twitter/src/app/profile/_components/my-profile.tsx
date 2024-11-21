49717fffdd216b3df79fd1028d1f718956a4983f
a-shigemichi <shigemichi45@gmail.com>
2024-11-21 10:48:55 +0900

    [TS] Implement Loading and Error Pages for Data Fetching States.

--- ts/twitter/src/app/profile/_components/my-profile.tsx
+++ ts/twitter/src/app/profile/_components/my-profile.tsx
@@ -1,5 +1,4 @@
-"use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import {
   Box,
   VStack,
@@ -11,115 +10,74 @@ import {
   Tooltip,
   Text,
 } from "@chakra-ui/react";
-import {
-  findUserById,
-  FindUserByIdResponse,
-} from "@/lib/actions/find-user-by-id";
-import { ArrowBackIcon, CalendarIcon } from "@chakra-ui/icons";
+import { findUserById } from "@/lib/actions/find-user-by-id";
+import { GoArrowLeft } from "react-icons/go";
+import { RxCalendar } from "react-icons/rx";
 import { MyProfileTab } from "./my-profile-tab";
 
-enum ProfileFetchState {
-  Loading,
-  Error,
-  Success,
-}
-
-export const MyProfile: React.FC = () => {
-  const [userProfile, setUserProfile] = useState<FindUserByIdResponse | null>(
-    null
-  );
-  const [error, setError] = useState<string | null>(null);
-  const [joinDate, setJoinDate] = useState<string>("");
-  const [fetchState, setFetchState] = useState<ProfileFetchState>(
-    ProfileFetchState.Loading
-  );
+export const MyProfile: React.FC = async () => {
   const userPostCount: number = 0;
   const followingCount: number = 0;
   const followersCount: number = 0;
 
-  useEffect(() => {
-    const fetchUserProfile = async () => {
-      try {
-        setFetchState(ProfileFetchState.Loading);
-        const res = await findUserById({
-          user_id: `${process.env.NEXT_PUBLIC_USER_ID}`,
-        });
-        setUserProfile(res);
-        const date = new Date(res.created_at);
-        const formattedDate = new Intl.DateTimeFormat("en-US", {
-          month: "long",
-          year: "numeric",
-        }).format(date);
-        setJoinDate(formattedDate);
-        setFetchState(ProfileFetchState.Success);
-      } catch (err) {
-        setError(
-          err instanceof Error ? err.message : "Failed to fetch user profile"
-        );
-        setFetchState(ProfileFetchState.Error);
-      }
-    };
+  const userProfile = await findUserById({
+    user_id: `${process.env.NEXT_PUBLIC_USER_ID}`,
+  });
+  const date = new Date(userProfile.created_at);
+  const userJoinedDate = new Intl.DateTimeFormat("en-US", {
+    month: "long",
+    year: "numeric",
+  }).format(date);
 
-    fetchUserProfile();
-  }, []);
-
-  switch (fetchState) {
-    case ProfileFetchState.Loading:
-      // TODO: https://github.com/okuda-seminar/Twitter-Clone/issues/472 - Implement Loading and Error Pages for Data Fetching States.
-      return <Text mx="4px">Loading...</Text>;
-    case ProfileFetchState.Error:
-      return <Text mx="4px">Error: {error}</Text>;
-    case ProfileFetchState.Success:
-      return (
-        <VStack align="stretch">
-          <Flex mx="4px" gap="4">
-            <Tooltip label="Back" placement="bottom" fontSize="xs">
-              <IconButton
-                mt="5px"
-                bg="transparent"
-                borderRadius="full"
-                aria-label="arrow"
-                icon={<ArrowBackIcon />}
-              />
-            </Tooltip>
-            <Box mt="5px">
-              <Text fontSize="lg">{userProfile?.display_name}</Text>
-              <Text fontSize="xs" color="gray">
-                {userPostCount} post
-              </Text>
-            </Box>
-          </Flex>
-          <Divider />
-          <Flex mx="8px">
-            <Avatar size="xl" mx="8px" name={userProfile?.username}></Avatar>
-            <Button borderRadius="full" marginLeft="auto">
-              EditProfile
-            </Button>
-          </Flex>
-          <Flex direction="column">
-            <Box fontSize="2xl" mt="2px" mx="24px">
-              {userProfile?.display_name}
-            </Box>
-            <Box fontSize="sm" color="gray" mx="24px">
-              @{userProfile?.username}
-            </Box>
-          </Flex>
-          <Flex alignItems="center" color="gray" mx="24px" gap="2">
-            <CalendarIcon />
-            <Box fontSize="sm">Joined {joinDate}</Box>
-          </Flex>
-          <Flex fontSize="sm" mx="24px" gap="4">
-            <Flex gap="1">
-              <Box>{followingCount}</Box>
-              <Box color="gray">Following</Box>
-            </Flex>
-            <Flex gap="1">
-              <Box>{followersCount}</Box>
-              <Box color="gray">Followers</Box>
-            </Flex>
-          </Flex>
-          <MyProfileTab />
-        </VStack>
-      );
-  }
+  return (
+    <VStack align="stretch">
+      <Flex mx="4px" gap="4">
+        <Tooltip label="Back" placement="bottom" fontSize="xs">
+          <IconButton
+            mt="5px"
+            bg="transparent"
+            borderRadius="full"
+            aria-label="arrow"
+            icon={<GoArrowLeft />}
+          />
+        </Tooltip>
+        <Box mt="5px">
+          <Text fontSize="lg">{userProfile.display_name}</Text>
+          <Text fontSize="xs" color="gray">
+            {userPostCount} post
+          </Text>
+        </Box>
+      </Flex>
+      <Divider />
+      <Flex mx="8px">
+        <Avatar size="xl" mx="8px" name={userProfile.username}></Avatar>
+        <Button borderRadius="full" marginLeft="auto">
+          EditProfile
+        </Button>
+      </Flex>
+      <Flex direction="column">
+        <Box fontSize="2xl" mt="2px" mx="24px">
+          {userProfile.display_name}
+        </Box>
+        <Box fontSize="sm" color="gray" mx="24px">
+          @{userProfile.username}
+        </Box>
+      </Flex>
+      <Flex alignItems="center" color="gray" mx="24px" gap="2">
+        <RxCalendar />
+        <Box fontSize="sm">Joined {userJoinedDate}</Box>
+      </Flex>
+      <Flex fontSize="sm" mx="24px" gap="4">
+        <Flex gap="1">
+          <Box>{followingCount}</Box>
+          <Box color="gray">Following</Box>
+        </Flex>
+        <Flex gap="1">
+          <Box>{followersCount}</Box>
+          <Box color="gray">Followers</Box>
+        </Flex>
+      </Flex>
+      <MyProfileTab />
+    </VStack>
+  );
 };
