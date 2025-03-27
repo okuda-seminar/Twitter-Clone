213b807c64b8b859c9770113603eb9feebd0b773
a-shigemichi <shigemichi45@gmail.com>
2025-03-27 19:13:56 +0900

    [TS] Update ChakraUI to v3 for UI components in posts and profile directory.

--- ts/twitter/src/app/profile/_components/my-profile-tab.tsx
+++ ts/twitter/src/app/profile/_components/my-profile-tab.tsx
@@ -1,88 +1,107 @@
 "use client";
 
-import {
-  Divider,
-  Tab,
-  TabIndicator,
-  TabList,
-  TabPanel,
-  TabPanels,
-  Tabs,
-  useColorModeValue,
-} from "@chakra-ui/react";
+import { useColorModeValue } from "@/lib/components/ui/color-mode";
+import { Tabs } from "@chakra-ui/react";
 import type React from "react";
 
 export const MyProfileTab: React.FC = () => {
   return (
-    <Tabs position="relative" variant="unstyled" width="100%">
-      <TabList width="100%" color="gray">
+    <Tabs.Root
+      position="relative"
+      width="100%"
+      key="lines"
+      defaultValue="posts"
+    >
+      <Tabs.List width="100%" color="gray" position="relative">
         {/* TODO: https://github.com/okuda-seminar/Twitter-Clone/issues/471
         - Refactor My Profile Tab to Create Custom Component and Implement Light Mode Support. */}
-        <Tab
+        <Tabs.Trigger
+          value="posts"
           flex={1}
-          _hover={{ background: useColorModeValue("gray.100", "transparent") }}
-          _selected={{ color: "white" }}
+          _hover={{
+            background: { base: "colors.gray.100", _dark: "transparent" },
+          }}
+          _selected={{ color: useColorModeValue("black", "white") }}
         >
           Posts
-        </Tab>
-        <Tab
+        </Tabs.Trigger>
+        <Tabs.Trigger
+          value="replies"
           flex={1}
-          _hover={{ background: useColorModeValue("gray.100", "transparent") }}
-          _selected={{ color: "white" }}
+          _hover={{
+            background: { base: "colors.gray.100", _dark: "transparent" },
+          }}
+          _selected={{ color: useColorModeValue("black", "white") }}
         >
           Replies
-        </Tab>
-        <Tab
+        </Tabs.Trigger>
+        <Tabs.Trigger
+          value="highlights"
           flex={1}
-          _hover={{ background: useColorModeValue("gray.100", "transparent") }}
-          _selected={{ color: "white" }}
+          _hover={{
+            background: { base: "colors.gray.100", _dark: "transparent" },
+          }}
+          _selected={{ color: useColorModeValue("black", "white") }}
         >
           Highlights
-        </Tab>
-        <Tab
+        </Tabs.Trigger>
+        <Tabs.Trigger
+          value="article"
           flex={1}
-          _hover={{ background: useColorModeValue("gray.100", "transparent") }}
-          _selected={{ color: "white" }}
+          _hover={{
+            background: { base: "colors.gray.100", _dark: "transparent" },
+          }}
+          _selected={{ color: useColorModeValue("black", "white") }}
         >
           Article
-        </Tab>
-        <Tab
+        </Tabs.Trigger>
+        <Tabs.Trigger
+          value="media"
           flex={1}
-          _hover={{ background: useColorModeValue("gray.100", "transparent") }}
-          _selected={{ color: "white" }}
+          _hover={{
+            background: { base: "colors.gray.100", _dark: "transparent" },
+          }}
+          _selected={{ color: useColorModeValue("black", "white") }}
         >
           Media
-        </Tab>
-        <Tab
+        </Tabs.Trigger>
+        <Tabs.Trigger
+          value="likes"
           flex={1}
-          _hover={{ background: useColorModeValue("gray.100", "transparent") }}
-          _selected={{ color: "white" }}
+          _hover={{
+            background: { base: "colors.gray.100", _dark: "transparent" },
+          }}
+          _selected={{ color: useColorModeValue("black", "white") }}
         >
           Likes
-        </Tab>
-      </TabList>
-      <TabIndicator mt="-1.5px" height="2px" bg="blue.500" borderRadius="1px" />
-      <Divider />
-      <TabPanels>
-        <TabPanel>
-          <p>Posts</p>
-        </TabPanel>
-        <TabPanel>
-          <p>Replies</p>
-        </TabPanel>
-        <TabPanel>
-          <p>Highlights</p>
-        </TabPanel>
-        <TabPanel>
-          <p>Articles</p>
-        </TabPanel>
-        <TabPanel>
-          <p>Medias</p>
-        </TabPanel>
-        <TabPanel>
-          <p>Likes</p>
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
+        </Tabs.Trigger>
+        <Tabs.Indicator
+          bottom="0"
+          position="absolute"
+          height="2px"
+          bg="blue.500"
+          borderRadius="1px"
+          zIndex="1"
+        />
+      </Tabs.List>
+      <Tabs.Content value="posts">
+        <p>Posts</p>
+      </Tabs.Content>
+      <Tabs.Content value="replies">
+        <p>Replies</p>
+      </Tabs.Content>
+      <Tabs.Content value="highlights">
+        <p>Highlights</p>
+      </Tabs.Content>
+      <Tabs.Content value="article">
+        <p>Articles</p>
+      </Tabs.Content>
+      <Tabs.Content value="media">
+        <p>Medias</p>
+      </Tabs.Content>
+      <Tabs.Content value="likes">
+        <p>Likes</p>
+      </Tabs.Content>
+    </Tabs.Root>
   );
 };
