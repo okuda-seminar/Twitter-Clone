3a4b84bde8bb002446cbc39003638e4f84dc37ee
harumaki <115347054+Amoonguses1@users.noreply.github.com>
2025-04-11 01:16:53 +0900

    [Go] Implement fake timelineitems repository.

--- go/internal/domain/repository/timelineitems.go
+++ go/internal/domain/repository/timelineitems.go
@@ -7,4 +7,13 @@ import (
 type TimelineItemsRepository interface {
 	SpecificUserPosts(userID string) ([]*entity.TimelineItem, error)
 	UserAndFolloweePosts(userID string) ([]*entity.TimelineItem, error)
+	CreatePost(userID, text string) (entity.TimelineItem, error)
+	DeletePost(postID string) error
+	CreateRepost(userID, postID string) (entity.TimelineItem, error)
+	DeleteRepost(postID string) error
+	CreateQuoteRepost(userID, postID, text string) (entity.TimelineItem, error)
+
+	// These methods are used to set or clear simulated operation-specific errors in tests.
+	SetError(key string, err error)
+	ClearError(key string)
 }
