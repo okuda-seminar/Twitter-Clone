d60b046cfef70964f608d68b1015289162e556b6
Rwatana <watawata.ryo1231@gmail.com>
2025-04-26 11:40:48 +0900

    [Go] Add CreateFollowship handler test that satisfies test isolation.

--- go/internal/controller/handler/types.go
+++ go/internal/controller/handler/types.go
@@ -10,12 +10,6 @@ type likePostRequestBody struct {
 	PostID uuid.UUID `json:"post_id,omitempty"`
 }
 
-// createFollowshipRequestBody is the type of the "CreateFollowship"
-// endpoint request body.
-type createFollowshipRequestBody struct {
-	TargetUserID string `json:"target_user_id"`
-}
-
 // createMutingRequestBody is the type of the "CreateMute"
 // endpoint request body.
 type createMutingRequestBody struct {
