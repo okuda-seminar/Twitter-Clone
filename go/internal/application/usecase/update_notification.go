a19f543b470dee2f6e6867cd216b086480c12ee9
harumaki <115347054+Amoonguses1@users.noreply.github.com>
2025-04-28 23:52:31 +0900

    [Go] Refactor CreatePost.

--- go/internal/application/usecase/update_notification.go
+++ go/internal/application/usecase/update_notification.go
@@ -2,6 +2,8 @@ package usecase
 
 import "x-clone-backend/internal/domain/entity"
 
+// TODO: https://github.com/okuda-seminar/Twitter-Clone/issues/720
+// - [Go] Fix notification usecase to handle uuid
 type UpdateNotificationUsecase interface {
 	SendNotification(userID, eventType string, timelineItem *entity.TimelineItem) error
 	SetChannel(userID string) (chan entity.TimelineEvent, error)
