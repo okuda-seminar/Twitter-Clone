0ee902d26d977b83e1de515215c25c36dcc9ffd9
harumaki <115347054+Amoonguses1@users.noreply.github.com>
2025-05-21 20:41:02 +0900

    [Go] Refactor CreateQuoteRepost.

---
+++ go/internal/application/usecase/create_quote_repost.go
@@ -0,0 +1,7 @@
+package usecase
+
+import "x-clone-backend/internal/domain/entity"
+
+type CreateQuoteRepostUsecase interface {
+	CreateQuoteRepost(userID, postID, text string) (entity.TimelineItem, error)
+}
