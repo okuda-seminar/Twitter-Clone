42caf44f784bed31db00ce9182f321c40f7dc6da
a-shigemichi <shigemichi45@gmail.com>
2025-06-05 09:24:03 +0900

    [TS] Adjust position of post modal when opened.

--- ts/twitter/src/app/(protected)/@modal/(.)compose/post/_components/post-modal/post-modal.tsx
+++ ts/twitter/src/app/(protected)/@modal/(.)compose/post/_components/post-modal/post-modal.tsx
@@ -53,80 +53,82 @@ export const PostModal: React.FC<PostModalProps> = ({ isIntercepted }) => {
       {/* TODO: https://github.com/okuda-seminar/Twitter-Clone/issues/664
     - Adjust position of post modal when opened. */}
       <Dialog.Backdrop />
-      <Dialog.Content
-        bg="black"
-        color="white"
-        w="400px"
-        minW="550px"
-        top="50px"
-      >
-        <Dialog.CloseTrigger
-          asChild
+      <Dialog.Positioner>
+        <Dialog.Content
+          bg="black"
           color="white"
-          position="absolute"
-          top="8px"
-          right="auto"
-          left="8px"
-          onClick={handleCloseButtonClick}
-          borderRadius="full"
-          p={2}
-          _hover={{
-            background: "gray.500",
-          }}
+          w="400px"
+          minW="550px"
+          top="50px"
         >
-          <CloseButton />
-        </Dialog.CloseTrigger>
-        <Dialog.Body pt="30px" pb={4}>
-          <form action={formAction}>
-            {message !== undefined && (
-              <Text
-                fontSize="14px"
-                lineHeight="16px"
-                px="16px"
-                py="12px"
-                bg="error.primary"
-                borderRadius="8px"
-              >
-                {message}
-              </Text>
-            )}
-            <Flex mt={8}>
-              <Avatar.Root size="md" mr={3}>
-                <Avatar.Fallback name={user ? user?.displayName : ""} />
-              </Avatar.Root>
-              <Textarea
-                data-testid="text"
-                name="text"
-                ref={textAreaRef}
-                value={postText}
-                onChange={handleTextAreaChange}
-                placeholder="What is happening?!"
-                border="none"
-                resize="none"
-                minH="100px"
-                _focus={{ boxShadow: "none" }}
-                fontSize="xl"
-              />
-            </Flex>
-            <Box borderTop="1px solid" borderColor="gray.600" mt={4} pt={4}>
-              <Flex justifyContent="space-between">
-                <IconButton
-                  // TODO: https://github.com/okuda-seminar/Twitter-Clone/issues/407
-                  // - Enhance Post Functionality: Add Image Attachment Feature.
-                  aria-label="Add image"
-                  variant="ghost"
-                  color="blue.400"
-                  borderRadius="full"
-                  _hover={{ bg: "whiteAlpha.200" }}
+          <Dialog.CloseTrigger
+            asChild
+            color="white"
+            position="absolute"
+            top="8px"
+            right="auto"
+            left="8px"
+            onClick={handleCloseButtonClick}
+            borderRadius="full"
+            p={2}
+            _hover={{
+              background: "gray.500",
+            }}
+          >
+            <CloseButton />
+          </Dialog.CloseTrigger>
+          <Dialog.Body pt="30px" pb={4}>
+            <form action={formAction}>
+              {message !== undefined && (
+                <Text
+                  fontSize="14px"
+                  lineHeight="16px"
+                  px="16px"
+                  py="12px"
+                  bg="error.primary"
+                  borderRadius="8px"
                 >
-                  <MediaIcon />
-                </IconButton>
-                <PostButton isDisabled={isPostButtonDisabled} />
+                  {message}
+                </Text>
+              )}
+              <Flex mt={8}>
+                <Avatar.Root size="md" mr={3}>
+                  <Avatar.Fallback name={user ? user?.displayName : ""} />
+                </Avatar.Root>
+                <Textarea
+                  data-testid="text"
+                  name="text"
+                  ref={textAreaRef}
+                  value={postText}
+                  onChange={handleTextAreaChange}
+                  placeholder="What is happening?!"
+                  border="none"
+                  resize="none"
+                  minH="100px"
+                  _focus={{ boxShadow: "none" }}
+                  fontSize="xl"
+                />
               </Flex>
-            </Box>
-          </form>
-        </Dialog.Body>
-      </Dialog.Content>
+              <Box borderTop="1px solid" borderColor="gray.600" mt={4} pt={4}>
+                <Flex justifyContent="space-between">
+                  <IconButton
+                    // TODO: https://github.com/okuda-seminar/Twitter-Clone/issues/407
+                    // - Enhance Post Functionality: Add Image Attachment Feature.
+                    aria-label="Add image"
+                    variant="ghost"
+                    color="blue.400"
+                    borderRadius="full"
+                    _hover={{ bg: "whiteAlpha.200" }}
+                  >
+                    <MediaIcon />
+                  </IconButton>
+                  <PostButton isDisabled={isPostButtonDisabled} />
+                </Flex>
+              </Box>
+            </form>
+          </Dialog.Body>
+        </Dialog.Content>
+      </Dialog.Positioner>
     </Dialog.Root>
   );
 };
