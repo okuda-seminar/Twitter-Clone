41bb84fce3801cbdeff190ee6e18f718e1e50154
TakayaShirai <s78taka03@gmail.com>
2025-06-25 01:03:33 +0900

    [Go] Write tests in LikePost.

--- go/internal/controller/handler/like_post.go
+++ go/internal/controller/handler/like_post.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	usecase "x-clone-backend/internal/application/usecase/api"
@@ -24,16 +23,18 @@ func (h *LikePostHandler) LikePost(w http.ResponseWriter, r *http.Request, userI
 
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&body)
+	// TODO: https://github.com/okuda-seminar/Twitter-Clone/issues/776
+	// - Update OpenAPI Schema to Support More Conditions for LikePost.
 	if err != nil {
-		http.Error(w, fmt.Sprintln("Request body was invalid."), http.StatusBadRequest)
+		http.Error(w, ErrDecodeRequestBody.Error(), http.StatusBadRequest)
 		return
 	}
 
 	err = h.likePostUsecase.LikePost(userID, body.PostId)
 	if err != nil {
-		http.Error(w, fmt.Sprintln("Could not create a like."), http.StatusInternalServerError)
+		http.Error(w, ErrCreateLike.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	w.WriteHeader(http.StatusCreated)
+	w.WriteHeader(http.StatusNoContent)
 }
