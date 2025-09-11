0480e861adc3f90f5f20ca451cc1f9ddd651fba0
harumaki <115347054+Amoonguses1@users.noreply.github.com>
2025-09-12 00:50:20 +0900

    [Go] Introduce cache.

--- go/internal/infrastructure/injector/users.go
+++ go/internal/infrastructure/injector/users.go
@@ -6,12 +6,12 @@ import (
 
 	"x-clone-backend/internal/domain/repository"
 	"x-clone-backend/internal/infrastructure/fake"
-	"x-clone-backend/internal/infrastructure/implementation"
+	"x-clone-backend/internal/infrastructure/rdb"
 )
 
 func InjectUsersRepository(db *sql.DB) repository.UsersRepository {
 	if testing.Testing() {
 		return fake.NewFakeUsersRepository()
 	}
-	return implementation.NewUsersRepository(db)
+	return rdb.NewUsersRepository(db)
 }
--- go/internal/infrastructure/rdb/rdb_timeline_items.go
+++ go/internal/infrastructure/rdb/rdb_timeline_items.go
@@ -1,8 +1,8 @@
-package repository
+package rdb
 
 import "x-clone-backend/internal/domain/entity"
 
-type TimelineItemsRepository interface {
+type RDBTimelineItemsRepository interface {
 	SpecificUserPosts(userID string) ([]*entity.TimelineItem, error)
 	UserAndFolloweePosts(userID string) ([]*entity.TimelineItem, error)
 	CreatePost(userID, text string) (entity.TimelineItem, error)
@@ -11,8 +11,4 @@ type TimelineItemsRepository interface {
 	DeleteRepost(postID string) (entity.TimelineItem, error)
 	CreateQuoteRepost(userID, postID, text string) (entity.TimelineItem, error)
 	TimelineItemByID(postID string) (*entity.TimelineItem, error)
-
-	// These methods are used to set or clear simulated operation-specific errors in tests.
-	SetError(key string, err error)
-	ClearError(key string)
 }
