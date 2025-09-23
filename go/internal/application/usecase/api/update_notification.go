81feb3c06d220bd2dc744046ce3bc99fba3304a1
motoshi-suzuki-hp <motoshi.suzuki.hp@gmail.com>
2025-09-23 21:06:13 +0900

    [Go] Abolish SSE.

--- go/internal/application/usecase/api/update_notification.go
+++ go/internal/application/usecase/api/update_notification.go
@@ -4,9 +4,4 @@ import "x-clone-backend/internal/domain/entity"
 
 type UpdateNotificationUsecase interface {
 	SendNotification(userID, eventType string, timelineItem *entity.TimelineItem) error
-	SetChannel(userID string) (chan entity.TimelineEvent, error)
-	DeleteChannel(userID string) error
-
-	SetError(err error)
-	ClearError()
 }
